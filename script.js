33abf0af620ded88c7935bce051b31d9b9839fb6
christianmesinas <48384755+christianmesinas@users.noreply.github.com>
2019-11-14 15:20:14 +0100

    nav styling update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+$(function () {
+  $(document).scroll(function(){
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop()> $nav.height());
+  });
+});
+$('.navbar-nav>li>a>').on('click', function(){
+});
+
 $(document).ready(function() {
 
     /* Every time the window is scrolled ... */
@@ -21,3 +31,26 @@ $(document).ready(function() {
    });
 
 });
+
+
+if ($('#back-to-top').length) {
+var scrollTrigger = 100, // px
+    backToTop = function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+            $('#back-to-top').addClass('show');
+        } else {
+            $('#back-to-top').removeClass('show');
+        }
+    };
+backToTop();
+$(window).on('scroll', function () {
+    backToTop();
+});
+$('#back-to-top').on('click', function (e) {
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop: 0
+    }, 700);
+});
+}
