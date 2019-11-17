28863696b93974d1ea0ebeaba5e30d823e808212
christianmesinas <48384755+christianmesinas@users.noreply.github.com>
2019-11-17 14:05:38 +0100

    back to top styling and fixes navigation

--- script.js
+++ script.js
@@ -31,26 +31,32 @@ $(document).ready(function() {
    });
 
 });
-
+// back to top jQuery code
 
 if ($('#back-to-top').length) {
-var scrollTrigger = 100, // px
-    backToTop = function () {
-        var scrollTop = $(window).scrollTop();
-        if (scrollTop > scrollTrigger) {
-            $('#back-to-top').addClass('show');
-        } else {
-            $('#back-to-top').removeClass('show');
-        }
-    };
-backToTop();
-$(window).on('scroll', function () {
+    var scrollTrigger = 100, // px
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop();
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
     backToTop();
-});
-$('#back-to-top').on('click', function (e) {
-    e.preventDefault();
-    $('html,body').animate({
-        scrollTop: 0
-    }, 700);
-});
+    $(window).on('scroll', function () {
+        backToTop();
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
 }
+
+// active link on click color change
+$("span").on('click', function (){
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+});
